5abb6882d7db3ed314e30377b1ffe65b42252494
Matthew Chess <mchess@umich.edu>
2014-10-28 17:31:52 -0400

    Initial Commit

---
+++ toe-tac-ticTests/toe_tac_ticTests.swift
@@ -0,0 +1,36 @@
+//
+//  toe_tac_ticTests.swift
+//  toe-tac-ticTests
+//
+//  Created by Matthew Chess on 10/28/14.
+//  Copyright (c) 2014 Matthew Chess. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class toe_tac_ticTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
