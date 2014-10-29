950507e06e370df567407b5a28e05897ebdb33d5
fuzzymath007 <mchess@umich.edu>
2014-10-28 22:38:51 -0400

    added var to keep track of what position each region is and if there is a winner.

--- toe-tac-tic/ViewController.swift
+++ toe-tac-tic/ViewController.swift
@@ -12,17 +12,55 @@ class ViewController: UIViewController {
     
     var turn = 1
     
+    var winner = 0
+    
+    // 0 is for empty, 1 is O and 2 is X
+    
+    var gameState = [0,0,0,0,0,0,0,0,0,0]
+    
+    let winningCombo = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    
     
     @IBOutlet var button0: UIButton!
 
     @IBAction func buttonPressed(sender: AnyObject) {
         
-        %
+        if(gameState[sender.tag]==0){
         
-        var image = UIImage(named: "cross.png")
+        var image = UIImage()
+        
+        if (turn % 2 == 0){
+        
+        image = UIImage(named: "cross.png")
+            
+            gameState[sender.tag] = 2
+            
+        }else{
+            
+        image = UIImage(named: "nought.png")
+            
+            gameState[sender.tag] = 1
+        
+        }
+            
+            for combination in winningCombo {
+                
+                if(gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] && gameState[combination[0]] != 0){
+                    
+                    winner = gameState[gameState[0]]
+                    
+                }
+            }
+            
+            if(winner != 0){
+                println("We have a winner")
+            }
+        
         sender.setImage(image, forState: .Normal)
         
-        
+        turn++
+            
+        }
         
     }
     
